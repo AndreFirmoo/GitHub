71209d0cf35218991ef16c0b83079cc4494cd617
André Felipe Jardim Firmo <andfrefirmo@outlook.com>
2019-08-27 23:04:19 -0300

    Create lauch screen and home

--- GitHub/ViewController.swift
+++ GitHub/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+   
+   
+    
+    private let dataSource = ["C#","Java","Swift","Kotlin","Javascript"]
 
+    @IBOutlet weak var pvLanguage: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pvLanguage.delegate = self
+        pvLanguage.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func BtnSelectedRow(_ sender: Any) {
+        let selectedRow = self.pvLanguage.selectedRow(inComponent: 0)
+        let value = dataSource[selectedRow]
+        print(selectedRow)
+        print(value)
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
 }
 
